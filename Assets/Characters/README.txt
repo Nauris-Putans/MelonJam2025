163df7e4c212d75fd733ae725ed8dceb0e0786b8
c4m3lion <c4m3lion2002@gmail.com>
2025-12-27 21:28:21 +0200

    sprites and gameplayu

--- Assets/Characters/README.txt
+++
@@ -1,5 +0,0 @@
-Splicing Guide
-
-When splicing use 16×33 pixels per frame
-Walking animations include a subtle 1-pixel head bob for added life
-Feet are aligned to the bottom row (bottom-center pivot recommended)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
+const SPEED = 120.0
 const JUMP_VELOCITY = -300.0
+
+var is_interaction_enabled = false;
+var interactible_type: String;
+var is_mask_toggled = false;
+
+@onready var tile_map_layer: TileMapLayer = $"../TileMapLayer"
+@onready var tile_map_layer_2: TileMapLayer = $"../TileMapLayer2"
+@onready var color_rect: ColorRect = $Camera2D/ColorRect
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -19,10 +27,13 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	if Input.is_action_just_pressed("interact"):
-		print_debug("I am gay!");
+	if Input.is_action_just_pressed("interact") && is_interaction_enabled:
+		print_debug("I am gay! but "+ interactible_type);
 	
 	if Input.is_action_just_pressed("toggle_mask"):
+		is_mask_toggled = !is_mask_toggled;
+		_toggle_overlay();
+		
 		print_debug("He is gay!");
 	
 	if direction > 0:
@@ -32,9 +43,9 @@ func _physics_process(delta: float) -> void:
 		
 	if is_on_floor():
 		if direction == 0:
-			animated_sprite_2d.play("idle");
+			_idle();
 		else:
-			animated_sprite_2d.play("walk");
+			_walk();
 	else:
 		animated_sprite_2d.play("jump");
 	
@@ -44,3 +55,56 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _can_interact(type: String):
+	is_interaction_enabled = true;
+	interactible_type = type;
+
+func _can_not_interact():
+	is_interaction_enabled = false;
+	interactible_type = "null";
+
+func _idle():
+	if is_mask_toggled:
+		animated_sprite_2d.play("masked_idle");
+	else:
+		animated_sprite_2d.play("idle");
+
+func _walk():
+	if is_mask_toggled:
+		animated_sprite_2d.play("masked_walk");
+	else:
+		animated_sprite_2d.play("walk");
+		
+func _toggle_overlay():
+	var tween := create_tween()
+
+	# Fade in (alpha 0 → 1)
+	tween.tween_property(
+		color_rect,
+		"color:a",
+		1.0,
+		0.5
+	)
+
+	# Wait until fade-in is done
+	await tween.finished
+
+	# Toggle tile maps AFTER screen is dark
+	if is_mask_toggled:
+		tile_map_layer.hide()
+		tile_map_layer_2.show()
+	else:
+		tile_map_layer.show()
+		tile_map_layer_2.hide()
+
+	# New tween for fade out
+	var tween_out := create_tween()
+	tween_out.tween_property(
+		color_rect,
+		"color:a",
+		0.0,
+		0.5
+	)
+
+	
